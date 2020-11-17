760ae9681920cdcb1e2bd590c27dd8d55bbf0ef7
Terra Roush <terra.roush@gmail.com>
2020-11-17 13:25:39 -0600

    building class created, private fields and public properties added. constructor begun

---
+++ Building.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Planner
+{
+    public class Building
+    {
+        private string _designer;
+        private DateTime _dateConstructed;
+        private string _address;
+        private string _owner;
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Volume
+        {
+            get
+            {
+                return Width * Depth * (3 * Stories);
+            }
+        }
+
+
+        public Building(string address)
+        {
+            Address = address;
+
+        }
+
+        public string Address { get; set; }
+    }
+
+
+}
